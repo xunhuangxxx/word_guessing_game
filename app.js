c47ac655e69516ab5d98851ac8ea0aea81766512
xunhuangxxx <xunhuangxxx@gmail.com>
2022-03-30 19:58:27 -0400

    reset function done

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const overlay = document.querySelector('#overlay');
 const phrases = ['red dress', 'two birds', 'go to park', 'old city', 'apple juice'];
 const hearts = document.querySelectorAll('.tries');
 
+const reset = document.createElement('button');
+reset.textContent = 'Reset';
+reset.style.display = 'none';
+reset.className = 'btn__reset';
+overlay.appendChild(reset);
+
 
 //Create a function to start the game
 startGame.addEventListener('click', function(){
@@ -63,14 +69,19 @@ function checkWin() {
     overlay.style.display = 'block';
     overlay.className = 'lose';
     overlay.querySelector('h2').textContent ="Sorry! Better luck next time.";
+    reset.style.display = 'block';
     return;
   }
   if(classLetter.length === classShow.length){
     overlay.style.display = 'block';
     overlay.className = 'win';
     overlay.querySelector('h2').textContent ='Congrats! You guessed the correct phrase!';
+    reset.style.display = 'block';
   }
+
 }
+
+
 
 //Add an event listener to the keyboard
 qwerty.addEventListener('click', (e)=>{
@@ -83,18 +94,35 @@ qwerty.addEventListener('click', (e)=>{
     if(letterFound === null && missed < hearts.length){
 
       missed += 1;
-      const img = document.createElement('img');
-      img.src = 'images/lostHeart.png';
-      img.style.height = '35px';
-      img.style.width = '30px';
+      const newImg = document.createElement('img');
+      newImg.src = 'images/lostHeart.png';
+      newImg.style.height = '35px';
+      newImg.style.width = '30px';
       hearts[missed-1].querySelector('img').remove();
-      hearts[missed-1].prepend(img);
+      hearts[missed-1].prepend(newImg);
     }
     checkWin();
   }
 });
 
-//create a reset button
+//create a reset function
+reset.addEventListener('click', function(){
+  overlay.style.display = 'none';
+  missed = 0;
+  const list = document.querySelectorAll('#phrase li');
+  const imgs = document.querySelectorAll('img');
+  const buttons = qwerty.querySelectorAll('button');
+  for(let i = 0; i< imgs.length; i++){
+    imgs[i].src = 'images/liveHeart.png';
+  }
+  for(let i = 0; i< list.length; i++){
+    list[i].remove();
+  }
+  for(let i = 0; i< buttons.length; i++){
+    buttons[i].className = '';
+    buttons[i].removeAttribute('disabled');
 
-const resetButton = document.createElement('button');
-overlay.appendChild(resetButton);
+  }
+  getRandomPhraseAsArray(phrases);
+  addPhraseToDisplay(phrases);
+})
